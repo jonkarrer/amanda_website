4f7f03a34b95fee95b60e3ad2bafef3cbf8680a1
jonkarrer <karrerjon@gmail.com>
2021-01-19 13:57:34 -0600

    add transition

--- dropdown-menu.js
+++ dropdown-menu.js
@@ -1,8 +1,11 @@
 const hamburgerIcon = document.getElementById("drop-down-menu");
 const exIcon = document.getElementById("drop-down_open-x-shape");
+const dropdownOverlay = document.querySelector(".drop-down_open-overlay");
 
 const activate = () => {
   hamburgerIcon.style.display = "none";
   exIcon.style.display = "flex";
+  dropdownOverlay.classList.toggle("open");
+  dropdownOverlay.classList.toggle("open-active");
 
 }
