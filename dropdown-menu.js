fd181c6cf6b59ef3843f81c942175e64f44f63e7
jonkarrer <karrerjon@gmail.com>
2021-01-19 12:46:53 -0600

    creating drop down

---
+++ dropdown-menu.js
@@ -0,0 +1,8 @@
+const hamburgerIcon = document.getElementById("drop-down-menu");
+const exIcon = document.getElementById("drop-down_open-x-shape");
+
+const activate = () => {
+  hamburgerIcon.style.display = "none";
+  exIcon.style.display = "flex";
+
+}
