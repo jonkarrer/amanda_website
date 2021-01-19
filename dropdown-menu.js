52b5b8ecc6e4209c1635a0c23ff412e4a6ef5586
jonkarrer <karrerjon@gmail.com>
2021-01-19 14:25:28 -0600

    fixed transistions

--- dropdown-menu.js
+++ dropdown-menu.js
@@ -1,11 +1,21 @@
 const hamburgerIcon = document.getElementById("drop-down-menu");
 const exIcon = document.getElementById("drop-down_open-x-shape");
 const dropdownOverlay = document.querySelector(".drop-down_open-overlay");
-
+const dropDownModule = document.querySelector(".drop-down_module");
+const main = document.querySelector("main");
 const activate = () => {
   hamburgerIcon.style.display = "none";
   exIcon.style.display = "flex";
   dropdownOverlay.classList.toggle("open");
   dropdownOverlay.classList.toggle("open-active");
-
+  dropDownModule.classList.toggle("open");
+  
+}
+const deActivate = () => {
+  hamburgerIcon.style.display = "flex";
+  exIcon.style.display = "none";
+  dropdownOverlay.classList.toggle("open");
+  dropdownOverlay.classList.toggle("open-active");
+  dropDownModule.classList.toggle("open");
+ 
 }
