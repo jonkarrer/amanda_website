3d26dc84e1ee60b417127f2ff8330b68efc6894e
jonkarrer <karrerjon@gmail.com>
2021-01-19 18:31:34 -0600

    finished dropdown

--- dropdown-menu.js
+++ dropdown-menu.js
@@ -2,20 +2,23 @@ const hamburgerIcon = document.getElementById("drop-down-menu");
 const exIcon = document.getElementById("drop-down_open-x-shape");
 const dropdownOverlay = document.querySelector(".drop-down_open-overlay");
 const dropDownModule = document.querySelector(".drop-down_module");
-const main = document.querySelector("main");
+const dropDownLinks = document.getElementById("drop-down-links");
 const activate = () => {
   hamburgerIcon.style.display = "none";
   exIcon.style.display = "flex";
   dropdownOverlay.classList.toggle("open");
   dropdownOverlay.classList.toggle("open-active");
   dropDownModule.classList.toggle("open");
-  
+  dropDownLinks.style.color = "white";
+  dropDownLinks.style.borderLeft= "white solid 5px";
 }
 const deActivate = () => {
   hamburgerIcon.style.display = "flex";
   exIcon.style.display = "none";
   dropDownModule.classList.toggle("open");
-  dropdownOverlay.classList.toggle("open");
-  dropdownOverlay.classList.toggle("open-active");
- 
+  dropdownOverlay.classList.toggle("open-active")
+  setTimeout(() => {
+    
+    dropdownOverlay.classList.toggle("open")
+  }, 600);
 }
