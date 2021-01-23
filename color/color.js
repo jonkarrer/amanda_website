c4eb188372fd9e95261352507eb5c693a63fa3e5
jonkarrer <karrerjon@gmail.com>
2021-01-23 12:00:16 -0600

    color page new

--- color/color.js
+++ color/color.js
@@ -1,10 +1,10 @@
 const fireEvent = evt => {
-  const target = evt.target;
+  const target = evt.target || evt.srcElement;
   target.style.transform = "rotateX(180deg)";
   target.parentElement.style.color = "black";
   target.firstChild.style.color = "black";
   target.firstChild.style.transform = "rotateX(180deg)";
 }
 const colorCardsArr = document.querySelectorAll("#colorCards");
-colorCardsArr.forEach((item) => item.addEventListener("click", fireEvent, false));
+colorCardsArr.forEach((item) => item.addEventListener("click", fireEvent));
 
