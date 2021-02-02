a6a5f8b8616bc7600b4121b4dae933496f1a3c59
jonkarrer <karrerjon@gmail.com>
2021-02-01 20:31:57 -0600

    new photos

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,21 +1,21 @@
 
 const mainElement = document.getElementById("portfolio-wrapper");
 const imageArr = [
-  "a1.jpg", "a2.jpg",
-  "b1.jpg", "b2.jpg",
-  "c1.jpg", "c2.jpg",
-  "d1.jpg", "d2.jpg",
-  "e1.jpg", "e2.jpg",
-  "f1.jpg", "f2.jpg",
-  "g1.jpg", "g2.jpg",
-  "h1.jpg", "h2.jpg",
-  "i1.jpg", "i2.jpg",
-  "j1.jpg", "j2.jpg",
-  "k1.jpg", "k2.jpg",
-  "l1.jpg", "l2.jpg",
-  "m1.jpg", "m2.jpg",
-  "n1.jpg", "n2.jpg",
-  "o1.jpg", "o2.jpg",
+  "./a1.jpg", "./a2.jpg",
+  "./b1.jpg", "./b2.jpg",
+  "./c1.jpg", "./c2.jpg",
+  "./d1.jpg", "./d2.jpg",
+  "./e1.jpg", "./e2.jpg",
+  "./f1.jpg", "./f2.jpg",
+  "./g1.jpg", "./g2.jpg",
+  "./h1.jpg", "./h2.jpg",
+  "./i1.jpg", "./i2.jpg",
+  "./j1.jpg", "./j2.jpg",
+  "./k1.jpg", "./k2.jpg",
+  "./l1.jpg", "./l2.jpg",
+  "./m1.jpg", "./m2.jpg",
+  "./n1.jpg", "./n2.jpg",
+  "./o1.jpg", "./o2.jpg",
 ]
 function injectImages() {
   for(let i = 0; i < imageArr.length; i += 2) {
@@ -29,5 +29,6 @@ function injectImages() {
     row.appendChild(imageTwo);
     mainElement.appendChild(row);
   };
+  return;
 };
 injectImages();
